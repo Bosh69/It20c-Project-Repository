eb548598e85514bb52b994a5b8b89711ec7629c8
Bosh69 <bvshermusic@gmail.com>
2024-09-25 16:08:24 +0800

    Simpleimplementation

---
+++ src/simpleimplementation/Simpleimplementation.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpleimplementation;
+
+/**
+ *
+ * @author ITLAB2-PC02-STUDENT
+ */
+public class Simpleimplementation {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("First element in array: " + numbers[0]);
+    }
+
+}
