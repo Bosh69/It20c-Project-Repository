202a40ac8edcee8a179cd79a369ce2cde38089f7
Bosh69 <bvshermusic@gmail.com>
2024-09-25 16:04:12 +0800

    IT20C

---
+++ src/it20c/bosh/It20CBosh.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it20c.bosh;
+
+/**
+ *
+ * @author ITLAB2-PC02-STUDENT
+ */
+public class It20CBosh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+       System.out.println(" Welcome to Data Structure and Algorithm");
+    }
+
+}
