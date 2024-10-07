33d960f89c9f7a3cd2a4a9b7785d449fe3ae5be2
andreejavier <20211241@nbsc.edu.ph>
2024-10-07 18:09:34 +0800

    Adding predefined songs to the list

---
+++ Main.java
@@ -0,0 +1,37 @@
+package topic_2_linkedlist_string;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+
+        // Adding predefined songs to the list
+        list.add("Reign Of Darkness by Thy Art Is Murder");
+        list.add("The Purest Strain Of Hate by Thy Art Is Murder");
+        list.add("Shadow Of Eternal Sin by Thy Art Is Murder");
+        list.add("Vile Creation by Thy Art Is Murder");
+        System.out.println("Songs by Thy Art Is Murder added to the list.");
+        System.out.println("Enter more song titles to add to the list (type 'done' to finish):");
+
+        // Loop to allow user input for adding nodes to the linked list
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("done")) {
+                break;
+            }
+            list.add(input);
+        }
+
+        System.out.println("Current Linked List:");
+        System.out.println("------------------------------");
+        list.printList();
+
+        
+
+        scanner.close();
+    }
+}
