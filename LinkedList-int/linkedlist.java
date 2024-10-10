bacaee5c7286c4771f4232d2b133eb36daf68ffd
Bosh69 <bvshermusic@gmail.com>
2024-10-10 21:52:13 +0800

    Added method to Add, Print, and Delete

--- LinkedList-int/linkedlist.java
+++ LinkedList-int/linkedlist.java
@@ -4,3 +4,48 @@ public class LinkedList {
 
         //head of the list
     private Node head;
+
+    //Method to add a new node
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+        //Method to print linked list
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+        //Method to delete a node by value
+    public void deleteByValue(int value) {
+        if (head == null) {
+            return;
+        }
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+
+                Node current = head;
+        while (current.next != null) {
+            if (current.next.data == value) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+}
