3ea77c06d95cbd4d0a8233f378153bab2c067ca1
Bosh69 <bvshermusic@gmail.com>
2024-10-10 21:45:32 +0800

    Initial commit on head of the list, Node class with init variables and constructor

---
+++ LinkedList-int/Node.java
@@ -0,0 +1,12 @@
+public class node {
+
+     //Initalizing variable data and Node next
+    int data;  
+    Node next;  
+    
+    // Constructor
+   Node(int data) {
+       this.data = data;
+       this.next = null;
+   }
+}
---
+++ LinkedList-int/linkedlist.java
@@ -0,0 +1,6 @@
+package topic_2_linkedlist_int;
+
+public class LinkedList {
+
+        //head of the list
+    private Node head;
