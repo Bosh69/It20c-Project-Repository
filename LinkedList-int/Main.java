d736a6e3e5f9ca7c1dcd2690f6f839d355693620
Bosh69 <bvshermusic@gmail.com>
2024-10-10 21:54:25 +0800

    Added User choice case 1

--- LinkedList-int/Main.java
+++ LinkedList-int/Main.java
@@ -10,4 +10,29 @@ public class Main {
         System.out.println("Good Day!");
         System.out.println("-------------------");
     }
+
+    while true {
+
+            System.out.println("\n Choose a number that you want to do: "
+                    + "\n 1. Add Items"
+                    + "\n 2. Delete an Item"
+                    + "\n 3. Move/Swap an Item"
+                    + "\n 4. Exit");
+
+            int userChoice = scanner.nextInt();
+
+            switch (userChoice) {
+                case 1:
+                    
+                    System.out.println("Enter atleast 5 elements you want to add: ");
+                    for (int i = 0; i < 5; i++) {
+                        int element = scanner.nextInt();
+                        linkedList.add(element);
+                    }
+                    System.out.println("Current Linked List:");
+                    linkedList.printList();
+                    break;
+            }
+    }
+
 }
