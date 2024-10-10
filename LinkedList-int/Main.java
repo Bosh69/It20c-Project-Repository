3b908d46b0156c56724b865ad921a9127902b987
Bosh69 <bvshermusic@gmail.com>
2024-10-10 21:50:08 +0800

    Added Scanner

---
+++ LinkedList-int/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static Main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.println("Good Day!");
+        System.out.println("-------------------");
+    }
+}
